308dec4c79ad7c82570ba715ba219223ea08a95d
sujipark <charmmming7@gmail.com>
2023-10-15 19:52:39 +0900

    6. Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import { useState } from "react";
 import styled from "styled-components";
 
-
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event; // event를 열어서 그 안의 currentTarget의 value를 가져온다.
-    setValue(value); // 그 value 값을 저장한다. 타입은 항상 string인지 검사한다.
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
+  const Container = styled.div`
+    color: ${props => props.theme.textColor};
+    background-color: ${props => props.theme.bgColor};
+    button {
+      color: ${props => props.theme.btnColor};
+    }
+  `;
+  const H1 = styled.h1`
+    color: ${props => props.theme.textColor};
+  `;
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} type="text" placeholder="username" onChange={onChange} />
-        <button type="submit">submit</button>
-      </form>
+      <Container>
+        <H1>Title</H1>
+      </Container>
     </div>
   );
 }
