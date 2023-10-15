019143252400f23d240ed59ee1039e19cab64ea5
sujipark <charmmming7@gmail.com>
2023-10-15 19:22:03 +0900

    5. Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
+
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event; // event를 열어서 그 안의 currentTarget의 value를 가져온다.
+    setValue(value); // 그 value 값을 저장한다. 타입은 항상 string인지 검사한다.
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" text="I'm here."></Circle>
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input value={value} type="text" placeholder="username" onChange={onChange} />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
