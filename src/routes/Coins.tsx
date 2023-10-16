92c9da42cc953615854940f9c5cebd1e82bcb96b
sujipark <charmmming7@gmail.com>
2023-10-16 20:22:31 +0900

    3. CRYPTO TRACKER - Home part2

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
   padding: 0 20px;
 `;
 
@@ -37,51 +41,81 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-]
+const Loader = styled.p`
+  text-align: center;
+`;
+
+// const coins = [
+//   {
+//     id: "btc-bitcoin",
+//     name: "Bitcoin",
+//     symbol: "BTC",
+//     rank: 1,
+//     is_new: false,
+//     is_active: true,
+//     type: "coin",
+//   },
+//   {
+//     id: "eth-ethereum",
+//     name: "Ethereum",
+//     symbol: "ETH",
+//     rank: 2,
+//     is_new: false,
+//     is_active: true,
+//     type: "coin",
+//   },
+//   {
+//     id: "hex-hex",
+//     name: "HEX",
+//     symbol: "HEX",
+//     rank: 3,
+//     is_new: false,
+//     is_active: true,
+//     type: "token",
+//   },
+// ]
+
+interface CoinInterface {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100)); // 100개 까지만 자르기
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {coins.map(coin => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>
-              {coin.name} &rarr;
-            </Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ): (
+        <CoinsList>
+          {coins.map(coin => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
