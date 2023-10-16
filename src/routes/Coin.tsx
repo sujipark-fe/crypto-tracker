a73a968a1715335c5238f3338142e6a2b0c90c01
sujipark <charmmming7@gmail.com>
2023-10-16 21:41:02 +0900

    5. CRYPTO TRACKER - Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,6 @@
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
+import { useEffect, useState, useSyncExternalStore } from "react";
 import { Container, Header, Title, Loader } from "./Coins";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
 
 interface RouteParams {
   coinId: string;
@@ -15,13 +14,30 @@ function Coin() {
   const { coinId } = useParams<RouteParams>();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(infoData);
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading"}</Title>
       </Header>
-      { loading ? <Loader>Loading...</Loader> : null }
+      { loading ? <Loader>Loading...</Loader> : <span>{info.hello}</span> }
     </Container>
   );
 }
